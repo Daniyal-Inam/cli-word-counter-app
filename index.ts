91d169fa1512db4f6adb4e8ffb794d497debbb61
Daniyal-Inam <daniyalinamuddin@gmail.com>
2024-04-23 00:57:33 +0500

    Created a Words Count App

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers: {
+  sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: chalk.blueBright.bold.italic(
+      "Enter your sentence to count the word: "
+    ),
+  },
+]);
+
+const words = answers.sentence.trim().split(" ");
+console.log("\n", chalk.greenBright.italic(words));
+
+console.log(
+  "\n",
+  chalk.yellowBright.bold.italic(
+    `Your sentence word count is: ${chalk.greenBright.italic(words.length)}`
+  )
+);
